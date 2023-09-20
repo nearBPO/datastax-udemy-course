65c8589d52605355f5b0b4d60e3bcd48f96516ce
lguedes-nearpbo <109820187+lguedes-nearpbo@users.noreply.github.com>
2023-09-20 14:55:42 -0300

    adding a proxy to access stargate to avoid the CORS message

--- vue-client/src/modules/sensors/api.ts
+++ vue-client/src/modules/sensors/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const AstraAPI = axios.create({
-    baseURL: `https://${import.meta.env.VITE_ASTRA_DB_ID}-${import.meta.env.VITE_ASTRA_DB_REGION
-        }.apps.astra.datastax.com/api/rest/v2/keyspaces/${import.meta.env.VITE_ASTRA_DB_KEYSPACE
-        }`,
+    baseURL: `/stargate/api/rest/v2/keyspaces/${import.meta.env.VITE_ASTRA_DB_KEYSPACE}`,
     headers: {
         "X-Cassandra-Token": `${import.meta.env.VITE_ASTRA_DB_APPLICATION_TOKEN}`,
     },
--- vue-client/vite.config.ts
+++ vue-client/vite.config.ts
@@ -1,10 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const env = loadEnv(
+  'all',
+  process.cwd()
+);
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/stargate': {
+        target: `https://${env.VITE_ASTRA_DB_ID}-${env.VITE_ASTRA_DB_REGION}.apps.astra.datastax.com`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/stargate/, ''),
+      },
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
